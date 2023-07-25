2f1cd5063244cff08849deb8ccd43d163b5b3e32
Jacob Beran <37278219+jakeberan@users.noreply.github.com>
2023-07-24 21:15:57 -0500

    initial logic for beautiful soup

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import math
 import pandas as pd
 import streamlit as st
 
+
+
 """
 # Welcome to Streamlit!
 
@@ -36,3 +38,17 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+
+# web scraping
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+print(results.prettify())
